41f8141f1264e0229eae519c9754331f000f2a01
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-08-17 09:24:00 +0200

    Removed superfluous/unused classes

--- src/classes/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './input/skill.dto';
-export * from './input/learningUnit.dto';
-export * from './input/skill-map.dto';
-
--- src/classes/input/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './skill.dto';
-export * from './learningUnit.dto';
-export * from './skill-map.dto';
-
