1ae25a2aa6532731dc8ea53c1065b4f3cc502d9b
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-09-08 06:30:45 +0200

    perf(Planner): Greedy strategy to improve performance
    If goal consists of multiple skills or have children, the greedy strategy searches for local optima and merge them together. The result will still be valid and good, but a global optimum won't be guaranteed anymore.
    
    This improves the performance drastically (up to 98% for complicated cases). closes #1

--- src/lib/fastDownward/fastDownward.ts
+++ src/lib/fastDownward/fastDownward.ts
@@ -164,3 +164,84 @@ export function findOptimalLearningPath<LU extends LearningUnit>({
 
 	return search(initialState, goal, skills, luProvider, fnCost, fnHeuristic);
 }
+
+/**
+ * Searches for an (optimal) path to learn the desired Skills (goal) based on the given knowledge, but uses an more greedy approach to discover child skills of the goal.
+ *
+ * As this approach uses a greedy-strategy, it's not guaranteed that the returned path is optimal.
+ * However, the algorithm will detect local optimums (for each partial goal / child of the goal) and will avoid to learn LearningUnits multiple times.
+ * Thus, the result should be a valid path and still optimal in many cases.
+ *
+ * By extending LU (must also be considered at the cost function and the heuristic) the algorithm
+ * can be adapted to work with different LearningUnit types.
+ *
+ * @param knowledge The skills that are already known by the learner.
+ * @param goal The skills that should be learned.
+ * @param skills The set of all skills (independent of what was already learned and what should be learned).
+ * @param lus The set of all LearningUnits.
+ * @param fnCost Function to calculate the costs of reaching a Node based on an operation performed on its predecessor.
+ * @param fnHeuristic Heuristic function to estimate the cost of reaching the goal from a given state.
+ * @returns An array of LearningUnits that represent the optimal path to learn the desired skills, or null if there is no solution.
+ */
+export async function findGreedyLearningPath<LU extends LearningUnit>({
+	knowledge,
+	goal,
+	skills,
+	lus,
+	fnCost,
+	fnHeuristic
+}: {
+	knowledge: Skill[];
+	goal: Skill[];
+	skills: ReadonlyArray<Skill>;
+	lus: LU[];
+	fnCost?: CostFunction<LU>;
+	fnHeuristic?: HeuristicFunction<LU>;
+}) {
+	/**
+	 * Iterate through the goal child by child.
+	 * For each child, find the optimal path and add the learned skills to the knowledge before learning the next child.
+	 * Compose the paths to a single path.
+	 * It's not guaranteed that this path is optimal, but it's guaranteed that it's a valid path.
+	 */
+	// TODO: Only children of the first layer are discovered, we could split the goal further
+	const flattenGoal: Skill[] = [];
+	goal.forEach(skill => {
+		if (skill.nestedSkills.length > 0) {
+			flattenGoal.push(
+				...skill.nestedSkills.map(childId => skills.find(skill => skill.id === childId)!)
+			);
+		} else {
+			flattenGoal.push(skill);
+		}
+	});
+
+	const pathResult: LU[] = [];
+	for (const child of flattenGoal) {
+		// Find local optimal path for the current partial goal
+		const path = findOptimalLearningPath({
+			knowledge,
+			goal: [child],
+			skills,
+			lus,
+			fnCost,
+			fnHeuristic
+		});
+
+		const pathLuIds = await path;
+		if (pathLuIds) {
+			// Glue partial paths together and add learned skills to the knowledge to avoid learning them twice
+			pathResult.push(...pathLuIds);
+			const learnedSkills = pathLuIds
+				.map(lu => lus.find(l => l.id === lu.id)!)
+				.flatMap(lu => lu.teachingGoals)
+				.map(skillId => skills.find(skill => skill.id === skillId)!);
+			knowledge = [...knowledge, ...learnedSkills];
+		} else {
+			// There exist no path for one of the children, so there is no path for the whole goal
+			return null;
+		}
+	}
+
+	return pathResult;
+}
