1fe04b3a1e7425924f9ef8687b14ab11dd4796b4
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-09-08 10:23:49 +0200

    chore: disable Jest preset; extend .gitignore
    * Jest preset seemed to block collecting coverage data -> disabled
    * Added output (folder of Junit/Coverage results) to .gitignore file

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,26 @@
 /* eslint-disable */
 export default {
-  displayName: 'skills',
-  jest: {
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-      '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    collectCoverageFrom: ['**/*.(t|j)s'],
-    testEnvironment: 'node',
-  },
+	displayName: "skills",
+	jest: {
+		moduleFileExtensions: ["js", "json", "ts"],
+		rootDir: "src",
+		testRegex: ".*\\.spec\\.ts$",
+		transform: {
+			"^.+\\.(t|j)s$": "ts-jest"
+		},
+		collectCoverageFrom: ["**/*.(t|j)s"],
+		testEnvironment: "node"
+	},
 
-  preset: './jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/skills',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+	// preset: './jest.preset.js',
+	testEnvironment: "node",
+	transform: {
+		"^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
+	},
+	moduleFileExtensions: ["ts", "js", "html"],
+	coverageDirectory: "./coverage/skills",
+	testMatch: [
+		"<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
+		"<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)"
+	]
 };
