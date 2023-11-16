59eed958f3f0f7773d9472af7f15de968f489b0c
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-11-16 13:41:40 +0100

    test(Planner): Scenario tests based on DigiMedia example

--- src/lib/pathPlanner.spec.ts
+++ src/lib/pathPlanner.spec.ts
@@ -362,6 +362,501 @@ describe("Path Planer", () => {
 				expectPath(path, [["lu:1", "lu:2", "lu:3"]], 3);
 			});
 		});
+
+		describe("Scenarios - DigiMedia", () => {
+			let [ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10, ch11, ch12]: Skill[] = [];
+			let digiMediaSkillMap: Skill[];
+			let digiMediaLUs: LearningUnit[];
+
+			// Creation of many test objects in a collapsable function ;-)
+			beforeAll(() => {
+				// Create Skills 1-202
+				const digiMediaSkills: Skill[] = [...Array(202).keys()].map(index => ({
+					id: `sk:${index + 1}`,
+					repositoryId: "1",
+					nestedSkills: []
+				}));
+				ch1 = {
+					id: "ch:1",
+					repositoryId: "1",
+					nestedSkills: [...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => `sk:${value}`)]
+				};
+				ch2 = {
+					id: "ch:2",
+					repositoryId: "1",
+					nestedSkills: [
+						...[
+							13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+							31, 32, 33, 34, 35, 36, 37, 38, 39
+						].map(value => `sk:${value}`)
+					]
+				};
+				ch3 = {
+					id: "ch:3",
+					repositoryId: "1",
+					nestedSkills: [
+						...[40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51].map(
+							value => `sk:${value}`
+						)
+					]
+				};
+				ch4 = {
+					id: "ch:4",
+					repositoryId: "1",
+					nestedSkills: [
+						...[
+							52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+							70, 71, 72, 73, 74, 75, 76, 77
+						].map(value => `sk:${value}`)
+					]
+				};
+				ch5 = {
+					id: "ch:5",
+					repositoryId: "1",
+					nestedSkills: [
+						...[78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91].map(
+							value => `sk:${value}`
+						)
+					]
+				};
+				ch6 = {
+					id: "ch:6",
+					repositoryId: "1",
+					nestedSkills: [
+						...[92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105].map(
+							value => `sk:${value}`
+						)
+					]
+				};
+				ch7 = {
+					id: "ch:7",
+					repositoryId: "1",
+					nestedSkills: [
+						...[
+							106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+							120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133,
+							134, 135, 136, 137
+						].map(value => `sk:${value}`)
+					]
+				};
+				ch8 = {
+					id: "ch:8",
+					repositoryId: "1",
+					nestedSkills: [
+						...[138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150].map(
+							value => `sk:${value}`
+						)
+					]
+				};
+				ch9 = {
+					id: "ch:9",
+					repositoryId: "1",
+					nestedSkills: [
+						...[151, 152, 153, 154, 155, 156, 157, 158, 159, 160].map(
+							value => `sk:${value}`
+						)
+					]
+				};
+				ch10 = {
+					id: "ch:10",
+					repositoryId: "1",
+					nestedSkills: [
+						...[
+							161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174,
+							175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185
+						].map(value => `sk:${value}`)
+					]
+				};
+				ch11 = {
+					id: "ch:11",
+					repositoryId: "1",
+					nestedSkills: [
+						...[186, 187, 188, 189, 190, 191, 192, 193, 194].map(value => `sk:${value}`)
+					]
+				};
+				ch12 = {
+					id: "ch:12",
+					repositoryId: "1",
+					nestedSkills: [
+						...[195, 196, 197, 198, 199, 200, 201, 202].map(value => `sk:${value}`)
+					]
+				};
+				digiMediaSkillMap = [
+					...digiMediaSkills,
+					ch1,
+					ch2,
+					ch3,
+					ch4,
+					ch5,
+					ch6,
+					ch7,
+					ch8,
+					ch9,
+					ch10,
+					ch11,
+					ch12
+				];
+				digiMediaLUs = [
+					newLearningUnit(digiMediaSkillMap, "lu:1", [], ["sk:1", "sk:2"]),
+					newLearningUnit(digiMediaSkillMap, "lu:2", ["sk:2"], ["sk:3", "sk:4", "sk:5"]),
+					newLearningUnit(digiMediaSkillMap, "lu:3", ["sk:5"], ["sk:6", "sk:7", "sk:8"]),
+					newLearningUnit(digiMediaSkillMap, "lu:4", ["sk:8"], ["sk:9"]),
+					newLearningUnit(digiMediaSkillMap, "lu:5", ["sk:4"], ["sk:10"]),
+					newLearningUnit(digiMediaSkillMap, "lu:6", ["sk:9"], ["sk:11"]),
+					newLearningUnit(digiMediaSkillMap, "lu:7", ["sk:11"], ["sk:12"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:8",
+						["sk:12"],
+						["sk:13", "sk:14", "sk:15"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:9",
+						["sk:13"],
+						["sk:16", "sk:17", "sk:18"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:10",
+						["sk:13"],
+						["sk:19", "sk:20", "sk:21"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:11",
+						["sk:13"],
+						["sk:22", "sk:23", "sk:24"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:12",
+						["sk:13"],
+						["sk:25", "sk:26", "sk:27"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:13",
+						["sk:11"],
+						["sk:28", "sk:29", "sk:30"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:14", ["sk:25"], ["sk:31"]),
+					newLearningUnit(digiMediaSkillMap, "lu:15", ["sk:4"], ["sk:32", "sk:33"]),
+					newLearningUnit(digiMediaSkillMap, "lu:16", [], ["sk:34", "sk:35"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:17",
+						["sk:7"],
+						["sk:36", "sk:37", "sk:38", "sk:39"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:18", ["sk:4"], ["sk:40", "sk:41"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:19",
+						["sk:41"],
+						["sk:42", "sk:43", "sk:44", "sk:45"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:20",
+						["sk:9"],
+						["sk:46", "sk:47", "sk:48"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:21",
+						["sk:47", "sk:41"],
+						["sk:49", "sk:50", "sk:51"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:22",
+						[],
+						["sk:52", "sk:53", "sk:54", "sk:55"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:23",
+						["sk:25", "sk:52"],
+						["sk:56", "sk:57", "sk:58", "sk:59", "sk:60"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:24", [], ["sk:61", "sk:62", "sk:63"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:25",
+						["sk:27", "sk:63"],
+						["sk:64", "sk:65"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:26",
+						["sk:56"],
+						["sk:66", "sk:67", "sk:68"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:27",
+						["sk:66"],
+						["sk:69", "sk:70", "sk:71", "sk:72"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:28", [], ["sk:73", "sk:74"]),
+					newLearningUnit(digiMediaSkillMap, "lu:29", [], ["sk:75", "sk:76", "sk:77"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:30",
+						["sk:25", "sk:26"],
+						["sk:78", "sk:79", "sk:80"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:31", ["sk:80"], ["sk:81", "sk:82"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:32",
+						["sk:26"],
+						["sk:83", "sk:84", "sk:85", "sk:86", "sk:87"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:33", ["sk:86"], ["sk:88", "sk:89"]),
+					newLearningUnit(digiMediaSkillMap, "lu:34", [], ["sk:90", "sk:91"]),
+					newLearningUnit(digiMediaSkillMap, "lu:35", [], ["sk:92", "sk:93", "sk:94"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:36",
+						["sk:93"],
+						["sk:95", "sk:96", "sk:97"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:37",
+						["sk:95", "sk:96", "sk:97"],
+						["sk:98", "sk:99", "sk:100"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:38",
+						["sk:93", "sk:99"],
+						["sk:101", "sk:102", "sk:103", "sk:104"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:39", ["sk:93"], ["sk:105"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:40",
+						["sk:43"],
+						["sk:106", "sk:107", "sk:108", "sk:109"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:41", ["sk:108"], ["sk:110", "sk:111"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:42",
+						["sk:108"],
+						["sk:112", "sk:113", "sk:114"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:43",
+						["sk:108"],
+						["sk:115", "sk:116", "sk:117", "sk:118", "sk:119"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:44",
+						["sk:108"],
+						["sk:120", "sk:121", "sk:122"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:45",
+						["sk:108"],
+						["sk:123", "sk:124", "sk:125"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:46",
+						["sk:108"],
+						["sk:126", "sk:127", "sk:128", "sk:129"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:47",
+						["sk:108"],
+						["sk:130", "sk:131", "sk:132", "sk:133"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:48",
+						["sk:108"],
+						["sk:134", "sk:135", "sk:136", "sk:137"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:49", [], ["sk:138", "sk:139"]),
+					newLearningUnit(digiMediaSkillMap, "lu:50", [], ["sk:140", "sk:141", "sk:142"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:51",
+						[],
+						["sk:143", "sk:144", "sk:145", "sk:146"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:52",
+						[],
+						["sk:147", "sk:148", "sk:149", "sk:150"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:53",
+						["sk:146"],
+						["sk:151", "sk:152", "sk:153"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:54",
+						["sk:151"],
+						["sk:154", "sk:155", "sk:156", "sk:157"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:55",
+						["sk:154"],
+						["sk:158", "sk:159", "sk:160"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:56", [], ["sk:161", "sk:162"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:57",
+						[],
+						["sk:163", "sk:164", "sk:165", "sk:166"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:58",
+						["sk:161"],
+						["sk:167", "sk:168", "sk:169"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:59",
+						["sk:161", "sk:167"],
+						["sk:170", "sk:171", "sk:172"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:60",
+						["sk:161"],
+						["sk:173", "sk:174", "sk:175", "sk:176"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:61",
+						["sk:161"],
+						["sk:177", "sk:178", "sk:179", "sk:180"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:62", ["sk:161"], ["sk:181", "sk:182"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:63",
+						["sk:161"],
+						["sk:183", "sk:184", "sk:185"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:64", [], ["sk:186", "sk:187"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:65",
+						["sk:186"],
+						["sk:188", "sk:189", "sk:190"]
+					),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:66",
+						["sk:190"],
+						["sk:191", "sk:192", "sk:193"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:67", ["sk:193"], ["sk:194"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:68",
+						["sk:12"],
+						["sk:195", "sk:196", "sk:197"]
+					),
+					newLearningUnit(digiMediaSkillMap, "lu:69", ["sk:197"], ["sk:198", "sk:199"]),
+					newLearningUnit(
+						digiMediaSkillMap,
+						"lu:70",
+						["sk:198"],
+						["sk:200", "sk:201", "sk:202"]
+					)
+				];
+			});
+
+			it("Greedy path for Chapter 4", async () => {
+				const path = await getPath({
+					skills: digiMediaSkillMap,
+					learningUnits: digiMediaLUs,
+					goal: [ch4],
+					optimalSolution: false,
+					contextSwitchPenalty: 1.2
+				});
+
+				// Maybe further alternatives possible, add them if necessary
+				expectPath(
+					path,
+					[
+						[
+							"lu:22",
+							"lu:1",
+							"lu:2",
+							"lu:3",
+							"lu:4",
+							"lu:6",
+							"lu:7",
+							"lu:8",
+							"lu:12",
+							"lu:23",
+							"lu:24",
+							"lu:25",
+							"lu:26",
+							"lu:27",
+							"lu:28",
+							"lu:29"
+						]
+					],
+					17.6
+				);
+			});
+
+			it("Optimal path for Chapter 4", async () => {
+				const path = await getPath({
+					skills: digiMediaSkillMap,
+					learningUnits: digiMediaLUs,
+					goal: [ch4],
+					optimalSolution: false,
+					contextSwitchPenalty: 1.2
+				});
+
+				// Maybe further alternatives possible, add them if necessary
+				expectPath(
+					path,
+					[
+						[
+							"lu:22",
+							"lu:1",
+							"lu:2",
+							"lu:3",
+							"lu:4",
+							"lu:6",
+							"lu:7",
+							"lu:8",
+							"lu:12",
+							"lu:23",
+							"lu:24",
+							"lu:25",
+							"lu:26",
+							"lu:27",
+							"lu:28",
+							"lu:29"
+						]
+					],
+					17.6
+				);
+			});
+		});
 	});
 
 	describe("computeSuggestedSkills", () => {
@@ -681,7 +1176,7 @@ function newLearningUnit(
 	requiredSkills: string[],
 	teachingGoals: string[],
 	suggestedSkills: { weight: number; skill: string }[] = []
-) {
+): LearningUnit {
 	const suggestions: { weight: number; skill: Skill }[] = [];
 	if (suggestedSkills.length > 0) {
 		for (const suggestion of suggestedSkills) {
