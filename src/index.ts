2bc35db71ec3e0a5ce39ef05f65d427b03ff972a
alamoush@sse.uni-hildesheim.de <alamoush@sse.uni-hildesheim.de>
2024-02-14 00:51:02 +0100

    feat(new): 21-nm-concept-composite-learning-units Apply the new concept of composite learning units as discussed in issue #21 in nm-concept (Concept of CompositeUnits) (Missing updates)
    Issue #21

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./lib/types";
 export * from "./lib/fastDownward/fdTypes";
 export * from "./lib/pathPlanner";
 export * from "./lib/fastDownward/missingSkillDetection";
+export * from "./lib/compositeLearningUnit";
