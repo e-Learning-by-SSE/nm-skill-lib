ec30b1567e28201c1ac5a60854e4ef27bdc5a31f
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-06-20 09:56:22 +0200

    perf(pathPlanner): Directly use of a set while filtering for useful LUs
    Benefits:
    * Avoids unnecessary creation of collection: array -> set -> array
    * Filtering for duplicates LUs early

--- src/lib/pathPlanner.ts
+++ src/lib/pathPlanner.ts
@@ -479,6 +479,7 @@ export function filterOutOfScopeLus<LU extends LearningUnit>(
         if (knowledge.map(skill => skill.id).includes(skill.id)) {
             continue;
         }
+
         // Find learning units that teach the current required skill
         const lus = learningUnits.filter(learningUnit =>
             learningUnit.teachingGoals.some(sk => sk.id == skill.id)
